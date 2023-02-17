faa959c3699d69958e4c91c9504ef74f33a6c94b
Mammi17 <hadjaramamane61@gmail.com>
2023-02-17 21:15:12 +0000

    task of variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+/**
+ * main - print lowercase alphabet excluding e and q
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char alph;
+
+	for (alph = 'a'; alph <= 'z'; alph++)
+	{
+		if (alph != 'e' && alph != 'q')
+			putchar(alph);
+	}
+	putchar('\n');
+	return (0);
+}
