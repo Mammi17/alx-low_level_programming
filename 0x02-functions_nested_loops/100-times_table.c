66021cb44b9f1bfce310acf009ec224c915faf2d
Mammi17 <hadjaramamane61@gmail.com>
2023-02-21 17:23:46 +0000

    task 0-16

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,32 +14,29 @@ void print_times_table(int n)
 		for (numb = 0; numb <= n; numb++)
 		{
 			_putchar('0');
-			_putchar(',');
-			_putchar(' ');
-		}
-
-		for (mul = 1; mul <= n; mul++)
-		{
-			_putchar(',');
-			_putchar(' ');
-
-			prod = numb * mul;
-
-			if (prod <= 99)
-				_putchar(' ');
-			if (prod <= 9)
-				_putchar(' ');
-			if (prod >= 100)
+			for (mul = 1; mul <= n; mul++)
 			{
-				_putchar((prod / 100) + '0');
-				_putchar(((prod / 10) % 10) + '0');
-			}
-			else if (prod <= 90 && prod >= 10)
-			{
-				_putchar((prod / 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+				
+				prod = numb * mul;
+				
+				if (prod <= 99)
+					_putchar(' ');
+				if (prod <= 9)
+					_putchar(' ');
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + '0');
+					_putchar(((prod / 10) % 10) + '0');
+				}
+				else if (prod <= 99 && prod >= 10)
+				{
+					_putchar((prod / 10) + '0');
+				}
 				_putchar((prod % 10) + '0');
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
