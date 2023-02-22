e27cff7f4d2a98dc141b74e99cf86ab9ea45d0f0
Mammi17 <hadjaramamane61@gmail.com>
2023-02-22 01:19:24 +0000

    task 0-16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,31 +14,26 @@ int main(void)
 	c = 2;
 	d = 10000000000;
 	e = b + c;
-	for (a = 0; a < 98; a++)
+	printf("%lu, %lu", b, c);
+	for (a = 2; a < 98; a++)
 	{
-		if (a == 0)
-			printf("%lu", b);
-		else if (a == 1)
-			printf(", %lu", c);
-		else if (e > d || i > 0 || j > 0)
+		if (e > d || i > 0 || j > 0)
 		{
 			up = e / d;
 			up1 = e % d;
 			up2 = i + j + up;
-			i = j;
-			j = up2;
-			b = c;
-			c = up1;
-			printf(", %lu%lu", j, c);
+			i = j, j = up2;
+			b = c, c = up1;
+			printf("%lu%010lu", j, c);
 		}
-
 		else
 		{
 			up1 = c + b;
-			b = c;
-			c = up1;
-			printf(", %lu", c);
+			b = c, c = up1;
+			printf("%lu", c);
 		}
+		if (a != 97)
+			printf(", ");
 	}
 	printf("\n");
 	return (0);
