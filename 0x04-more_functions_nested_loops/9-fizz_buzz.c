36e715ef019a3607938d8d8d8c68aa6e00610a1a
Mammi17 <hadjaramamane61@gmail.com>
2023-02-24 00:29:57 +0000

    task 0-12

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		putchar(' ');
+		/*putchar(' ');*/
 		if (i % 3 == 0)
 		{
 			printf("Fizz");
@@ -29,6 +29,7 @@ int main(void)
 		{
 			printf("%d", i);
 		}
+		printf(" ");
 	}
 	printf("\n");
 	return (0);
