9598da9fca02aa3f880a7abfaf47b4fbb5a9d9d9
Mammi17 <hadjaramamane61@gmail.com>
2023-03-03 03:32:35 +0000

    task 0-12

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,35 +7,35 @@
 
 void print_number(int n)
 {
-	int pow, neg, hold;
+	int pow, neg, hod;
 	
 	neg = 0;
 	pow = 1;
-	hold = n;
+	hod = n;
 	if (n < 0)
 	{
 		_putchar('-');
 		neg = 1;
 	}
-	while (hold > 9 || hold < -9)
+	while (hod > 9 || hod < -9)
 	{
 		pow *= 10;
-		hold /= 10;
+		hod /= 10;
 	}
 	while (pow > 0)
 	{
 		if (pow > 9)
 		{
-			if (!neg)
+			if (neg != 0)
 				_putchar((n / pow % 10) + '0');
 			else
-				_putchar((n / pow % 10) * -1 + '0');
+				_putchar((n / pow % 10) * - 1 + '0');
 			pow /= 10;
 		}
 		if (pow == 1)
 		{
 			if (neg)
-				_putchar((n % 10) * -1 = '0');
+				_putchar((n % 10) * - 1 = '0');
 			else
 				_putchar((n % 10) + '0');
 			pow = 0;
