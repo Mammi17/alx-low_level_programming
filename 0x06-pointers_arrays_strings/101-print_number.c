b63dea512ecc47db149632692fa8395134edcc06
Mammi17 <hadjaramamane61@gmail.com>
2023-03-04 01:51:41 +0000

    task 0-12

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,14 +7,14 @@
 
 void print_number(int n)
 {
-	int pod;
+	unsigned int pod;
 
 	pod = n;
-	/*if (n < 0)
+	if (n < 0)
 	{
 		_putchar('-');
 		pod = -pod;
-	}*/
+	}
 	if ((pod / 10) > 0)
 		_print_number(pod / 10);
 	_putchar((pod % 10) + '0');
