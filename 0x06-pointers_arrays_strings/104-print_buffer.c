e806e1559a221a56cf61cc64fa78d211ee4f11b7
Mammi17 <hadjaramamane61@gmail.com>
2023-03-04 02:49:54 +0000

    task 0-12

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,11 +1,12 @@
 #include "main.h"
 #include <stdio.h>
-/*
+/**
  * print_buffer- that prints a buffer
  * @b: a pointer
  * @size: an integer
  * Return: a void
  */
+
 void print_buffer(char *b, int size)
 {
 	int a, c;
@@ -15,12 +16,12 @@ void print_buffer(char *b, int size)
 		printf("%08x: ", a);
 		for (c = 0; c < 10; c++)
 		{
+			if ((c % 2) != 0 && index != 0)
+				printf(" ");
 			if ((c + a) >= size)
 				printf("  ");
 			else
 				printf("%02x", *(b + a + c));
-			if ((c % 2) != 0 && index != 0)
-				printf(" ");
 		}
 		for (c = 0; c < 10; c++)
 		{
