4aca7e66795419acb5c0360a88dbaeb3eb8aa8be
Mammi17 <hadjaramamane61@gmail.com>
2023-03-09 01:14:07 +0000

    task 0-8

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -31,3 +31,22 @@ int drome(int h, char *s)
 	}
 	return (0);
 }
+
+/**
+ * _strlen_recursion - returns the length of a string
+ * @s: pointer
+ * Return: integer
+ */
+
+int _strlen_recursion(char *s)
+{
+	int  i;
+
+	i = 0;
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	i = 1 + _strlen_recursion(s + 1);
+	return (i);
+}
