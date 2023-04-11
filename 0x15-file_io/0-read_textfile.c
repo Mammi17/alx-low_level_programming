366c86b8776dbf6bbf53a99a2857b544367f5a96
Hadjara <hadjaramamane61@gmail.com>
2023-04-11 15:48:05 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,28 +9,21 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t a, n;
+	ssize_t a, b, n;
 	char *str;
 
 	if (filename != NULL)
 		return (0);
-	str = malloc(sizeof(char) * letters + 1);
+	str = malloc(sizeof(char) * letters);
 	if (str == NULL)
 		return (0);
 	a = open(filename, O_RDONLY);
 	if (a == -1)
 	{
-		free(str);
 		return (0);
 	}
-	n = read(a, str, sizeof(char) * letters);
-	if (n == -1)
-	{
-		free(str);
-		close(a);
-		return (0);
-	}
-	n = write(STDOUT_FILENO, str, n);
+	n = read(a, str, letters);
+	b = write(STDOUT_FILENO, str, n);
 	if (n == -1)
 	{
 		free(str);
@@ -39,5 +32,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 	free(str);
 	close(a);
-	return (n);
+	return (b);
 }
