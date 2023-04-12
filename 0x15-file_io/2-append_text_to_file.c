0f3a1842e70fc0069518c80927d27f4bb797ddbc
Hadjara <hadjaramamane61@gmail.com>
2023-04-11 17:49:58 -0700

    task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,14 +15,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 	a = open(filename, O_WRONLY | O_APPEND);
-	if (a == -1 || b != c)
-		return (-1);
 	if (text_content)
 	{
 		while (text_content[c])
 			c++;
 		b = write(a, text_content, c);
 	}
+	if (a == -1 || b != c)
+		return (-1);
 	close(a);
 	return (1);
 }
