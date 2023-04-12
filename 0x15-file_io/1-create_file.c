7904a74ef79174fbd0f421a0b3af7a200435c662
Hadjara <hadjaramamane61@gmail.com>
2023-04-11 17:36:37 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,11 +14,9 @@ int create_file(const char *filename, char *text_content)
 	c = 0;
 	if (filename == NULL)
 		return (-1);
-
 	a = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (a < 0)
 		return (-1);
-
 	if (text_content)
 	{
 		c = 0;
