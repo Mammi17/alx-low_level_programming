d5ef249feaa8e5c6aed4380b5f6461e6732aec12
Hadjara <hadjaramamane61@gmail.com>
2023-04-14 13:23:05 -0700

    task 0-4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,6 +1,8 @@
 #include <elf.h>
 #include "main.h"
 #include <stdio.h>
+#define REV ( n ) ((n << 24 ) | (((n >> 16 ) << 24 ) >> 16 ) | \
+(((n << 16 ) >> 24 ) << 16 ) | (n >> 24 ))
 /**
 * control- verify the file to check if is a ELF
 * @id: the ELF struct
