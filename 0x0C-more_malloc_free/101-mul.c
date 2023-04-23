62e1d9104b85a5b00c7272569de2d5991edd7269
Hadjara <hadjaramamane61@gmail.com>
2023-04-22 19:09:38 -0700

    task 0-5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -28,10 +28,10 @@ int main(int argc, char **argv)
 				exit(98);
 			}
 		}
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mul = num1 * num2;
+		printf("%lu\n", mul);
 	}
-	num1 = atoi(argv[1]);
-	num2 = atoi(argv[2]);
-	mul = num1 * num2;
-	printf("%lu\n", mul);
 	return (0);
 }
