0675817743dd331345a6ff8a73087dff70458177
Hadjara <hadjaramamane61@gmail.com>
2023-04-23 19:00:02 -0700

    task 0-5

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,61 +14,43 @@ int main(int argc, char *argv[])
 	int *str, i, j, b = 0, ln1, ln2, c, n1, n2;
 
 	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+	{ printf("Error\n");
+		exit(98); }
 	num1 = argv[1], num2 = argv[2];
 	j = 0;
 	while (num1[j] != '\0')
-	{
-		if (!digit(num1[j]))
-		{
-			printf("Error\n");
-			exit(98);
-		}
-		j++;
-	}
+	{ if (!digit(num1[j]))
+		{ printf("Error\n");
+			exit(98); }
+		j++; }
 	j = 0;
 	while (num2[j] != '\0')
-	{
-		if (!digit(num2[j]))
-		{
-			printf("Error\n");
-			exit(98);
-		}
-		j++;
-	}
-	ln1 = length(num1);
-	ln2 = length(num2);
+	{ if (!digit(num2[j]))
+		{ printf("Error\n");
+			exit(98); }
+		j++; }
+	ln1 = length(num1), ln2 = length(num2);
 	c = ln1 + ln2 + 1;
 	str = malloc(sizeof(int) * c);
 	if (str == NULL)
 		return (1);
-
 	for (j = 0; j < c; j++)
 		str[j] = 0;
 	for (ln1 -= 1; ln1 >= 0; ln1--)
-	{
-		n1 = num1[ln1] - '0';
+	{ n1 = num1[ln1] - '0';
 		i = 0;
 		for (ln2 = length(num2) - 1; ln2 >= 0; ln2--)
-		{
-			n2 = num2[ln2] - '0';
+		{ n2 = num2[ln2] - '0';
 			i += str[ln1 + ln2 + 1] + (n1 * n2);
 			str[ln1 + ln2 + 1] = i % 10;
-			i /= 10;
-		}
+			i /= 10; }
 		if (i > 0)
-			str[ln1 + ln2 + 1] += i;
-	}
+			str[ln1 + ln2 + 1] += i; }
 	for (j = 0; j < c - 1; j++)
-	{
-		if (str[j])
+	{ if (str[j])
 			b = 1;
 		if (b)
-			_putchar(str[j] + '0');
-	}
+			_putchar(str[j] + '0'); }
 	if (!b)
 		_putchar('0');
 	_putchar('\n');
