babdd9b8c6217bf6018140e4e75a9c895138304a
hadjara <hadjaramamane61@gmail.com>
2023-05-04 20:06:13 -0700

    task 0-14

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -20,25 +20,3 @@ listint_t *add_nodeint(listint_t **head, const int n)
 	(*head) = ncurrent;
 	return (ncurrent);
 }
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    listint_t *head;
-
-    head = NULL;
-    add_nodeint(&head, 0);
-    add_nodeint(&head, 1);
-    add_nodeint(&head, 2);
-    add_nodeint(&head, 3);
-    add_nodeint(&head, 4);
-    add_nodeint(&head, 98);
-    add_nodeint(&head, 402);
-    add_nodeint(&head, 1024);
-    print_listint(head);
-    return (0);
-}
