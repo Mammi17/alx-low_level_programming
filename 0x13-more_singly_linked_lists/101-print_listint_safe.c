c141bbbcecec334bbf0aa9b3804db02aeab3e7bd
hadjara <hadjaramamane61@gmail.com>
2023-05-06 18:56:13 -0700

    task 0-14

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -20,11 +20,11 @@ size_t print_listint_safe(const listint_t *head)
 		if (b > 0)
 		{
 			head = head->next;
-			a++;
 		}
 		else
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+			a++;
 			break;
 		}
 	}
