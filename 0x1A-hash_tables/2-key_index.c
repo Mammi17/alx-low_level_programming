0325fcc976ac11872f466f41fe34c05186c64e5d
hadjara <hadjaramamane61@gmail.com>
2023-06-09 19:04:26 -0700

    task 0-7

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,13 +2,15 @@
 #include "hash_tables.h"
 
 /**
- * key_index - that gives you the index of a key.
- * @key: a key
- * @size: size of table hash
- * Return: a unsigned integer
- */
+  * key_index - create a index for the key in the hash table
+  * @size: size of the hash table
+  * @key: the key
+  * Return: the index of the key
+  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	return (hash_djb2(key) % size);
+	unsigned long int cle = 0;
 
+	cle = hash_djb2(key) % size;
+	return (cle);
 }
